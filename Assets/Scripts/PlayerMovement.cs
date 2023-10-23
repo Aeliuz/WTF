3b0a958bb9d4d2c7714628b726f464cf12657ff0
Sofie Reyes B <sofie.reyes97@gmail.com>
2023-10-23 12:05:15 +0200

    added scripts again lol

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    public float playerMaxspeed;
+    Vector2 userInput;
+    Vector2 velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        userInput.x = Input.GetAxisRaw("Horizontal");
+        userInput.y = Input.GetAxisRaw("Vertical");
+        userInput.Normalize();
+        velocity = userInput * playerMaxspeed;
+        rb2D.velocity = velocity;
+    }
+}
