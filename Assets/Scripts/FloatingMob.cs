72142f71d46e4d24648be706f58f67fbc6d13d11
Sofie Reyes B <sofie.reyes97@gmail.com>
2023-10-26 14:48:32 +0200

    I MADE THE MOB GO DOWN YAAAAAYYYY

--- Assets/Scripts/FloatingMob.cs
+++ Assets/Scripts/FloatingMob.cs
@@ -6,14 +6,43 @@ using UnityEngine;
 public class FloatingMob : MonoBehaviour
 {
     private Vector3 _startPosition;
+   
+    float playerSinking;
+    float NewYPos;
 
     void Start()
     {
         _startPosition = transform.position;
+        
     }
 
     void Update()
     {
-        transform.position = _startPosition + new Vector3(0f, Mathf.Sin(Time.time), 0.0f);
+
+        _startPosition.y = NewYPos;
+       //transform.position = new Vector3(_startPosition.x, sinkPosition, _startPosition.y);
+      
+       // sinkPosition -= _startPosition.y * sinkDepth * Time.deltaTime; 
+           
+
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            NewYPos = _startPosition.y - 0.5f;
+            transform.position = new Vector3(_startPosition.x, NewYPos, _startPosition.z);
+
+        }
+        else
+        {
+            transform.position = _startPosition + new Vector3(0f, MathF.Sin(Time.time), 0.0f);
+        }
+
+
+    }
+
+
 }
