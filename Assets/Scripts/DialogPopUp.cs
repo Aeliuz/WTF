a5fe5518522c81cfe3bec97fe78016d642d7fd39
Alice Nyman <117102070+alicenym@users.noreply.github.com>
2023-10-26 15:59:43 +0200

    pop up

--- Assets/Scripts/DialogPopUp.cs
+++ Assets/Scripts/DialogPopUp.cs
@@ -2,45 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bunny : MonoBehaviour
+public class DialogPopUp : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
-    
-    AudioSource audioSource;
-    public AudioClip audioClip;
 
-    // Start is called before the first frame update
+    SpriteRenderer spriteRenderer;
+
+    
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.enabled = false;
-        
-        audioSource = GetComponent<AudioSource>();
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
             spriteRenderer.enabled = true;
-            spriteRenderer.GetComponentInChildren<SpriteRenderer>().enabled = true;
-            audioSource.Play();
         }
 
         Invoke("Disappear", 6f);
     }
 
-
     void Disappear()
     {
         spriteRenderer.enabled = false;
     }
-
 }
