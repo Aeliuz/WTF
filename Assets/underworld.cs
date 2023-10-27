b7f4f3188a24375dd9b1b3bbfc99591f5795d931
Johan Lamberth <johan.lamberth@gmail.com>
2023-10-27 08:47:51 +0200

    "finished"

---
+++ Assets/underworld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class underworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Movement.overworld = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
