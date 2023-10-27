5e9904c1c29f981404a432ce1e84466ffff292ab
Johan Lamberth <johan.lamberth@gmail.com>
2023-10-27 20:41:40 +0200

    Day 1 Patch

---
+++ Assets/setBool.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setBool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Movement.overworld = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
